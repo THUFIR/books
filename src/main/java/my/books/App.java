391ba121ac9bb12c3bd06822b28e5d0a3f311243
thufir <hawat.thufir@gmail.com>
2019-01-17 23:49:02 -0800

    incremental

--- src/main/java/my/books/App.java
+++ src/main/java/my/books/App.java
@@ -1,19 +1,15 @@
 package my.books;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 import java.util.Properties;
 import java.util.logging.Logger;
-import javax.xml.bind.JAXB;
-import javax.xml.transform.stream.StreamSource;
 import nu.xom.Builder;
 import nu.xom.Document;
-import nu.xom.ParsingException;
-import nu.xom.ValidityException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
+import java.nio.file.Paths;
 
 public class App {
 
@@ -28,9 +24,12 @@ public class App {
         properties.loadFromXML(App.class.getResourceAsStream("/properties.xml"));
         LOG.info(properties.toString());
         URI inputURI = new URI(properties.getProperty("html_input"));
+        URI outputURI = new URI(properties.getProperty("output"));
         File htmlInputFile = new File(inputURI);
         Document document = fileToDocument(htmlInputFile);
+        Files.write(Paths.get(outputURI), document.toXML().getBytes());
     }
+
 
     private Document fileToDocument(File file) throws Exception {
         XMLReader tagsoup = XMLReaderFactory.createXMLReader("org.ccil.cowan.tagsoup.Parser");
@@ -39,6 +38,5 @@ public class App {
         x = bob.build(file);
         return x;
     }
-
 
 }
