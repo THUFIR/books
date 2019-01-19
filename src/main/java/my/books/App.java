a6304d96e4a4bf56a6bc0c91e9a2503f53649e5f
thufir <hawat.thufir@gmail.com>
2019-01-19 00:49:21 -0800

    incremental

--- src/main/java/my/books/App.java
+++ src/main/java/my/books/App.java
@@ -7,7 +7,6 @@ import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class App {
 
